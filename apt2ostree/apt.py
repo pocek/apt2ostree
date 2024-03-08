0a252130ce0946e60cc730e9d535d5023cbdf93e
Krzysztof Kotlenga <k.kotlenga@sims.pl>
2024-03-08 11:47:38 +0100

    Allow to fix control too
    I would be surprised if this couldn't be improved. Also, the
    patch_control rule shall also live here.

--- apt2ostree/apt.py
+++ apt2ostree/apt.py
@@ -168,13 +168,13 @@ make_dpkg_info = Rule(
         tmpdir=$builddir/tmp/make_dpkg_info/$sha256sum;
         rm -rf "$$tmpdir";
         mkdir -p $$tmpdir/out/var/lib/dpkg/info;
-        ostree --repo=$ostree_repo checkout --repo=$ostree_repo -UH "$ref_base/control" "$$tmpdir/control";
+        ostree --repo=$ostree_repo checkout --repo=$ostree_repo -UH "$ref_control" "$$tmpdir/control";
         multi_arch=$$(awk '/^Multi-Arch:/ {print $$2}' $$tmpdir/control/control);
         if [ "$$multi_arch" = "same" ]; then
             architecture=$$(awk '/^Architecture:/ {print $$2}' $$tmpdir/control/control);
             suffix=":$$architecture";
         fi;
-        ostree --repo=$ostree_repo ls -R $ref_base/data --nul-filenames-only
+        ostree --repo=$ostree_repo ls -R $ref_data --nul-filenames-only
         | tr '\\0' '\\n' 
         | sed 's,^/$$,/.,' >$$tmpdir/out/var/lib/dpkg/info/$pkgname$$suffix.list;
         cd "$$tmpdir";
@@ -196,22 +196,22 @@ make_dpkg_info = Rule(
         ( cat control/control; echo Status: install ok unpacked; echo ) >status;
         ( cat control/control; echo ) >available;
         cd -;
-        ostree --repo=$ostree_repo commit -b "$ref_base/info" --tree=dir=$$tmpdir/out
+        ostree --repo=$ostree_repo commit -b "$ref_info" --tree=dir=$$tmpdir/out
             --no-bindings --orphan --timestamp=0 --owner-uid=0 --owner-gid=0
             --no-xattrs;
-        overwrite_if_changed $$tmpdir/status $builddir/$ref_base/status;
-        overwrite_if_changed $$tmpdir/available $builddir/$ref_base/available;
+        overwrite_if_changed $$tmpdir/status $builddir/$ref_control/status;
+        overwrite_if_changed $$tmpdir/available $builddir/$ref_control/available;
         rm -rf "$$tmpdir";
     """,
     restat=True,
     output_type=(str, str, OstreeRef),
     outputs=[
-        '$builddir/$ref_base/status',
-        '$builddir/$ref_base/available',
-        '$ostree_repo/refs/heads/$ref_base/info'],
+        '$builddir/$ref_control/status',
+        '$builddir/$ref_control/available',
+        '$ostree_repo/refs/heads/$ref_info'],
     order_only=["$ostree_repo/config"],
-    inputs=["$ostree_repo/refs/heads/$ref_base/control",
-            "$ostree_repo/refs/heads/$ref_base/data"])
+    inputs=["$ostree_repo/refs/heads/$ref_control",
+            "$ostree_repo/refs/heads/$ref_data"])
 
 do_usrmove = Rule(
     "do_usrmove", """\
@@ -546,7 +546,7 @@ class Apt(object):
                         pkg['SHA256'][4:], os.path.basename(filename))
                     ref_base = ("deb/pool/" + aptly_pool_filename
                                 .replace('+', '_').replace('~', '_'))
-                    data, _ = download_deb.build(
+                    data, control = download_deb.build(
                         self.ninja, sha256sum=pkg['SHA256'], filename=filename,
                         aptly_pool_filename=aptly_pool_filename,
                         mirror=pkg['X-Archive-Root'],
@@ -556,11 +556,13 @@ class Apt(object):
                             self.ninja,
                             in_branch=data.ref,
                             out_branch=data.ref + '-usrmove')
-                    data = self.fix_package(pkg, data)
+                    data, control = self.fix_package(pkg, data, control)
                     all_data.append(data.filename)
+                    head, sep, tail = control.ref.rpartition('/')
+                    ref_info = f'{head}/info'
                     status, available, info = make_dpkg_info.build(
                         self.ninja, sha256sum=pkg['SHA256'],
-                        pkgname=pkg['Package'], ref_base=ref_base)
+                        pkgname=pkg['Package'], ref_data=data.ref, ref_control=control.ref, ref_info=ref_info)
                     all_status.append(status)
                     all_available.append(available)
                     all_info.append(info.filename)
@@ -598,7 +600,7 @@ class Apt(object):
                          "phony", inputs=image.filename)
         return image
 
-    def fix_package(self, pkg, data):
+    def fix_package(self, pkg, data, control):
         """
         Here we can apply quirks as required to get particular packages to
         install.
@@ -616,14 +618,14 @@ class Apt(object):
             # much of which isn't valid Python files.
             #
             # See also https://salsa.debian.org/python-team/applications/pylint/commit/28d9e9231f58ef9a1debeb4ae34f4d7441c36a67
-            return ostree_addfile.build(
+            data = ostree_addfile.build(
                 self.ninja, in_branch=data.ref,
                 prefix="/usr/share/python/bcep",
                 in_file=_find_file("quirks/pylint/pylint.bcep"),
                 out_branch=data.ref + "-fixed")
         elif pkgname == "apt" and version == "2.0.8":
             # https://bugs.launchpad.net/ubuntu/+source/apt/+bug/1968154/comments/16
-            return ostree_addfile.build(
+            data = ostree_addfile.build(
                 self.ninja, in_branch=data.ref,
                 prefix="/etc/kernel/postinst.d",
                 in_file=_find_file("quirks/apt/apt-auto-removal"),
@@ -631,13 +633,13 @@ class Apt(object):
         elif pkgname == "usrmerge":
             # Disable usrmerge.postinst, as we have done usrmove ourselves and
             # for some reason the usrmerge.postinst script fails to detect this.
-            return ostree_addfile.build(
+            data = ostree_addfile.build(
                 self.ninja, in_branch=data.ref,
                 prefix="/var/lib/dpkg/info",
                 in_file=_find_file("quirks/usrmerge/usrmerge.postinst"),
                 out_branch=data.ref + "-fixed")
         else:
-            return data
+            return data, control
 
 
 def parse_packages(stream):
